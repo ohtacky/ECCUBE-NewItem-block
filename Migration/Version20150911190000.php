06c545f2df404b0b8342bc012865e55fd1fca365
takashi <takashi@handmadecompany.jp>
2015-10-10 16:57:37 +0900

    インストール時にブロック追加

---
+++ Migration/Version20150911190000.php
@@ -0,0 +1,48 @@
+<?php
+/*
+* This file is part of EC-CUBE
+*
+* Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+* http://www.lockon.co.jp/
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class Version20150911190000 extends AbstractMigration
+{
+
+    public function up(Schema $schema)
+    {
+    }
+
+    public function down(Schema $schema)
+    {
+        //アンインストール時のDBブロックの削除処理
+        //アンインストール時のtwigの削除処理
+    }
+
+    public function postUp(Schema $schema)
+    {
+
+        $app = new \Eccube\Application();
+        $app->boot();
+        $deviceTypeId = '10';
+        $blockName = '新商品';
+        $fileName = 'newitem';
+        $datetime = date('Y-m-d H:i:s');
+        $logicFlg = '1';
+        $deletableFlg = '1';
+        $insert = "INSERT INTO dtb_block(
+                            device_type_id, block_name, file_name, create_date, update_date, logic_flg, deletable_flg)
+                    VALUES ('$deviceTypeId', '$blockName', '$fileName', '$datetime', '$datetime', '$logicFlg', '$deletableFlg'
+                            );";
+        $this->connection->executeUpdate($insert);
+
+    }
+}
--- PluginManager.php
+++ PluginManager.php
@@ -12,12 +12,22 @@
 namespace Plugin\NewItem;
 
 use Eccube\Plugin\AbstractPluginManager;
+use Symfony\Component\Filesystem\Filesystem;
 
 class PluginManager extends AbstractPluginManager
 {
 
     public function install($config, $app)
     {
+      $this->migrationSchema($app, __DIR__ . '/Migration', $config['code']);
+
+      $file = new Filesystem();
+      try {
+          $file->copy($app['config']['root_dir']. '/app/Plugin/NewItem/newitem.twig', $app['config']['root_dir']. '/app/template/default/Block/newitem.twig', true);
+          return true;
+      } catch (\Exception $e) {
+          return false;
+      }
     }
 
     public function uninstall($config, $app)
